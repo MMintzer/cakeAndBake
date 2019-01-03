dacca362697c09a21005413cc248b584f5178e42
Matt <mintzer.chemistry@gmail.com>
2019-01-03 12:34:44 -0500

    update navbar to display cakeCoins.  create new files for models necessary for shopping cart

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -4,7 +4,7 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
+const Navbar = ({handleClick, isLoggedIn, cakeCoins}) => (
   <div>
     <h1>Cake and Bake</h1>
     <nav>
@@ -13,6 +13,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           {/* The navbar will show these links after you log in */}
           <Link to="/home">Home</Link>
           <Link to="/products">Products</Link>
+          <Link to="/cakeCoin">{cakeCoins}</Link>
           <a href="#" onClick={handleClick}>
             Logout
           </a>
@@ -35,7 +36,8 @@ const Navbar = ({handleClick, isLoggedIn}) => (
  */
 const mapState = state => {
   return {
-    isLoggedIn: !!state.user.id
+    isLoggedIn: !!state.user.id,
+    cakeCoins: state.user.cakeCoins
   }
 }
 
