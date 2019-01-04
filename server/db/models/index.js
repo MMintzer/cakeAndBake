5cbcba5867bba102b2adb3a54e07341d488d0ce9
Matt <mintzer.chemistry@gmail.com>
2019-01-04 11:04:31 -0500

    define order and orderitem models.  add associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,19 +1,20 @@
 const User = require('./user')
 const Product = require('./product')
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
+const Order = require('./crder')
+const OrderItem = require('./orderItem')
+// VV associations VV
+User.hasMany(Order)
+Order.belongsTo(User)
 
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
+Product.hasMany(OrderItem)
+OrderItem.belongsTo(Product)
+
+Order.hasMany(OrderItem)
+OrderItem.belongsTo(Order)
+
 module.exports = {
   User,
-  Product
+  Product,
+  Order,
+  OrderItem
 }
